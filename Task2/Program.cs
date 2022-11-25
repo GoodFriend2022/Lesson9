26af8c5e3e3ad8d0a0d217784c89cad8eeb21b79
Uri_Klevcov <a-08-137.mail.ru>
2022-11-25 22:44:00 +0500

    Task2 sum numbers

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers(int m, int n)
+{
+    if (m > n) return 0;
+    int sum = m + SumNumbers(m + 1, n);
+    return sum;
+}
+
+int userM = Prompt("Введите первое число");
+int userN = Prompt("Введите второе число");
+System.Console.WriteLine(SumNumbers(userM, userN));
